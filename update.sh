ed1ce983de3703c2920a22434992bcdbc9c87d8b
Christoph <ckluente@thoughtworks.com>
2016-09-22 18:18:36 +0200

    new sigs

--- update.sh
+++ update.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 gpg --homedir=. --refresh-keys
 gpg --homedir=. --list-sigs | sig2dot > thoughtworks-sigs.dot
-if [[ "$unamestr" == 'Linux' ]]; then
-  springgraph < thoughtworks-sigs.dot > thoughtworks-sigs.png
-else
-  dot -T png -O thoughtworks-sigs.dot
-fi
+springgraph < thoughtworks-sigs.dot > thoughtworks-sigs.png
