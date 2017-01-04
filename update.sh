e8968486ed1a88d7f1c8b0c5fc0c674cfa44cb46
Christoph <ckluente@thoughtworks.com>
2017-01-04 22:42:27 +0100

    find more keys.
    download all unknown uids, but only include the thoughtworks keys
    in the graph

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-gpg --homedir=. --refresh-keys
-gpg --homedir=. --list-sigs | sig2dot > thoughtworks-sigs.dot
+gpg --homedir=. --refresh-keys thoughtworks.com
+for key in $(gpg --homedir=. --list-sigs --with-colons thoughtworks.com| grep 'User ID not found' | cut -d':' -f 5); do
+  gpg --homedir=. --recv-key $key
+done
+gpg --homedir=. --list-sigs --keyid-format 0xlong thoughtworks.com | sig2dot -a > thoughtworks-sigs.dot
 springgraph < thoughtworks-sigs.dot > thoughtworks-sigs.png
